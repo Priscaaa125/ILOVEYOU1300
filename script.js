43c45117d3c1334d7cc937596d9a32c3ef8a9f47
priscaadel <dellaprisca007@gmail.com>
2026-03-13 15:06:16 +0700

    animation for heart and footer text

---
+++ script.js
@@ -0,0 +1,34 @@
+const heartElement = document.getElementById('heart-text');
+const footerElement = document.getElementById('footer-text');
+
+const textPattern = "I LOVE YOU ";
+const footerString = "by: Prisca";
+
+const totalHeartChars = 2800; 
+const typingSpeedHeart = 1;  
+const typingSpeedFooter = 100; 
+
+let heartIndex = 0;
+let footerIndex = 0;
+
+function typeHeart() {
+    if (heartIndex < totalHeartChars) {
+        heartElement.innerHTML += textPattern[heartIndex % textPattern.length];
+        heartIndex++;
+        setTimeout(typeHeart, typingSpeedHeart);
+    } else {
+        
+        typeFooter();
+    }
+}
+
+function typeFooter() {
+    if (footerIndex < footerString.length) {
+        footerElement.innerHTML += footerString.charAt(footerIndex);
+        footerIndex++;
+        setTimeout(typeFooter, typingSpeedFooter);
+    }
+}
+
+
+typeHeart();
